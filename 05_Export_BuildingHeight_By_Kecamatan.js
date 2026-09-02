2346c8bb15c78aef339f1939833a688f18c9a851
Royhan Firdaus <royhanfirdaus255@gmail.com>
2026-09-03 06:10:22 +0800

    Create 05_Export_BuildingHeight_By_Kecamatan.js
    script untuk memanggil atau memunculkan kecamatan

---
+++ 05_Export_BuildingHeight_By_Kecamatan.js
@@ -0,0 +1,14 @@
+===========================================================
+//variabel untuk memanggil atau memunculkan per kecamatan//
+===========================================================
+  
+var namaKecamatan =
+'Balikpapan Selatan';
+
+//tinggal diganti dengan kecamatan anda//
+Balikpapan Barat
+Balikpapan Kota
+Balikpapan Selatan
+Balikpapan Tengah
+Balikpapan Utara
+Balikpapan Timur
